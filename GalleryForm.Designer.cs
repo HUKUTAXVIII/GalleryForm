5888471535a32d91f61c3be942f943280781f3a7
Никита Антоненко <nikita.dmitrovich@gmail.com>
2021-07-03 19:54:13 +0300

    Add functions to buttons

--- GalleryForm.Designer.cs
+++ GalleryForm.Designer.cs
@@ -29,22 +29,22 @@ namespace GalleryForm
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
             this.nextButton = new System.Windows.Forms.Button();
             this.prevButton = new System.Windows.Forms.Button();
-            this.panel1 = new System.Windows.Forms.Panel();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.imagePanel = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // pictureBox
             // 
-            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(776, 312);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.pictureBox.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pictureBox.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(776, 312);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
             // 
             // nextButton
             // 
@@ -55,6 +55,7 @@ namespace GalleryForm
             this.nextButton.TabIndex = 1;
             this.nextButton.Text = "-->";
             this.nextButton.UseVisualStyleBackColor = false;
+            this.nextButton.Click += new System.EventHandler(this.nextButton_Click);
             // 
             // prevButton
             // 
@@ -65,14 +66,15 @@ namespace GalleryForm
             this.prevButton.TabIndex = 2;
             this.prevButton.Text = "<--";
             this.prevButton.UseVisualStyleBackColor = false;
+            this.prevButton.Click += new System.EventHandler(this.prevButton_Click);
             // 
-            // panel1
+            // imagePanel
             // 
-            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.panel1.Location = new System.Drawing.Point(93, 330);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(614, 108);
-            this.panel1.TabIndex = 3;
+            this.imagePanel.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.imagePanel.Location = new System.Drawing.Point(93, 330);
+            this.imagePanel.Name = "imagePanel";
+            this.imagePanel.Size = new System.Drawing.Size(614, 108);
+            this.imagePanel.TabIndex = 3;
             // 
             // GalleryForm
             // 
@@ -80,24 +82,24 @@ namespace GalleryForm
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.imagePanel);
             this.Controls.Add(this.prevButton);
             this.Controls.Add(this.nextButton);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "GalleryForm";
             this.Text = "Gallery";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox;
         private System.Windows.Forms.Button nextButton;
         private System.Windows.Forms.Button prevButton;
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel imagePanel;
     }
 }
 
--- GalleryForm.cs
+++ GalleryForm.cs
@@ -16,14 +16,57 @@ namespace GalleryForm
         public GalleryForm()
         {
             InitializeComponent();
-            if (File.Exists("Images")) {
-                Directory.GetFiles("Images/").ToList().ForEach((image)=> { 
-                    
+            if (Directory.Exists("Images/")) {
+                Directory.GetFiles("Images/").ToList().ForEach((image)=> {
+                    Button imageLabel = new Button();
+                    imageLabel.BackgroundImage = (Image)Image.FromFile(image).Clone();
+                    imageLabel.BackgroundImageLayout = ImageLayout.Stretch;
+                    imageLabel.Size = new Size(90,90);
+                    imageLabel.Name = image;
+                    imageLabel.Click += (s, e) => {
+                        var img = s as Button;
+                        this.pictureBox.Image = img.BackgroundImage;
+                    };
+                    if (this.imagePanel.Controls.Count == 0)
+                    {
+                        imageLabel.Location = new Point(10, 10);
+                    }
+                    else {
+                        imageLabel.Location = new Point(this.imagePanel.Controls.OfType<Button>().Last().Location.X+101, 10);
+                    }
+
+
+                    this.imagePanel.Controls.Add(imageLabel);
                     
                 });
             }
 
 
+        }
+
+        private void nextButton_Click(object sender, EventArgs e)
+        {
+            var arr = this.imagePanel.Controls.OfType<Button>().ToArray();
+            if (arr.Last().Location.X >= this.imagePanel.Width-50)
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    arr[i].Location = new Point(arr[i].Location.X - 101, 10);
+                }
+            }
+
+        }
+
+        private void prevButton_Click(object sender, EventArgs e)
+        {
+            var arr = this.imagePanel.Controls.OfType<Button>().ToArray();
+            if (arr.First().Location.X < 0) 
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    arr[i].Location = new Point(arr[i].Location.X + 101, 10);
+                }
+            }
         }
     }
 }
